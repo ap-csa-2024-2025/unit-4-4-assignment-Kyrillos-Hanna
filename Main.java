abbf6de2a6f96a95e09ec59d13685bc6241cec07
Kyrillos-Hanna <kyrohanna90@gmail.com>
2024-11-12 11:20:10 -0800

    finished problem 1 and 2

--- Main.java
+++ Main.java
@@ -4,14 +4,28 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
-  }
+    Scanner sc = new Scanner(System.in);
 
-  public static boolean isPrime(int N)
-  {
-    // TODO: Replace and write your code below
-    return false;
+    System.out.println("Input String:" );
+    String sentence = sc.nextLine();
+    sentence = sentence.toLowerCase();
+    int times = 0;
+    String newString = "";
+
+    for (int i = 0; i <= sentence.length() - 1; i++) {
+      if (sentence.substring(i, i + 1).equals("p")) {
+        if (sentence.substring(i + 1, i + 2).equals("a") || sentence.substring(i + 1, i + 2).equals("e") || sentence.substring(i + 1, i + 2).equals("i") || sentence.substring(i + 1, i + 2).equals("o") || sentence.substring(i + 1, i + 2).equals("u")) {
+          times += 1;
+        }
+      }
+    }
+    System.out.println(times);
+
+    for (int i = 0; i <= sentence.length() - 1; i++) {
+      if (!(sentence.substring(i, i + 1).equals("e") || sentence.substring(i, i + 1).equals("t") || sentence.substring(i, i + 1).equals("a") || sentence.substring(i, i + 1).equals("i") || sentence.substring(i, i + 1).equals("o"))) {
+        newString = newString + sentence.substring(i, i + 1);
+      } 
+    }
+    System.out.println(newString);
   }
 }
